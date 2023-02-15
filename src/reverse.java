909c861b878d69a2dde3f449567bc9cdc0cc313f
EH2O <edwin.holgerssonolofsson@elev.ga.ntig.se>
2023-02-15 14:12:04 +0100

    Att använda string builder gjort att program körde mycket snabbare.

--- src/reverse.java
+++ src/reverse.java
@@ -5,9 +5,9 @@ public class reverse {
     public static void main(String[] args) {
         long start = System.nanoTime();
         Scanner in;
-        String fill = "src/loremIpsum.txt";
+        String fill = "lormeIpsum.txt";
         try {
-            in = new Scanner((new File("src/loremIpsum.txt")));
+            in = new Scanner((new File(fill)));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -18,10 +18,10 @@ public class reverse {
         StringBuilder reva = new StringBuilder();
 
         for (int i = 0; i < data.length(); i++) {
-            reva.append(data.charAt(data.length() - i));
+            reva.append(data.charAt(data.length() - i-1));
         }
         long test =  System.nanoTime();
-        System.out.println(start-test);
+        System.out.println(test-start);
 
     }
 }
